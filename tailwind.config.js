d5afa1c099cc67716e947c3419a3f75979df9650
Debosmit Choudhury <debosmit.choudhury@enggproject.com>
2022-01-16 19:03:25 +0530

    fix tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,7 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    screen : {
-      'lg': {'max': '992px'},
-      // => @media (max-width: 1023px) { ... }
-
-      'md': {'max': '660px'},
-      // => @media (max-width: 767px) { ... }
-    },
+    extend: {},
   },
   plugins: [],
 };
